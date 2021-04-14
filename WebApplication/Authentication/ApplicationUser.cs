12d6b46609fcf642f0a7b60c95f988fb8b0d41f3
CHRISTINE-KATHLEEN RÁCZ <christine.racz@stud.ubbcluj.ro>
2021-04-14 18:33:40 +0300

    WebAPI

---
+++ WebApplication/Authentication/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Xamarin.Essentials;
+
+namespace WebAPI.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //[Key]
+        //public override string Id { get; set; }
+        [ProtectedPersonalData]
+        public virtual string FirstName { get; set; }
+
+        [ProtectedPersonalData]
+        public virtual string LastName { get; set; }
+
+        [ProtectedPersonalData]
+        public float RatingGrade { get; set; }
+        // public Location UserLoc { get; set; }
+    }
+}
---
+++ WebApplication/Authentication/RegisterModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;  
+  
+namespace WebAPI.Authentication
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Telephone number is required")]
+        public string Telephone { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+
+    }
+}
